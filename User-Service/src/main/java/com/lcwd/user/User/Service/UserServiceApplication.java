c29f2e328b3047b57272ee61850eb1c0f9202dd6
Vishal-Ramisetty <vishalramisetty7@gmail.com>
2025-08-26 15:12:04 +0530

    Commit 6: Enable Eureka Client- User, Hotel and Rating Service(Removed @EnableEurekaClient- Autoconfigured with latest spring cloud in pom.xml)

--- User-Service/src/main/java/com/lcwd/user/User/Service/UserServiceApplication.java
+++ User-Service/src/main/java/com/lcwd/user/User/Service/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.lcwd.user.User.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//@EnableEurekaClient  - Use this for other versions of Spring Cloud Netflix
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
